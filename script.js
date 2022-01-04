5263da81d727607d9acfd3f6b98f8c2473cbc6c9
eugenejac3 <eugene.jackson9@gmail.com>
2022-01-04 16:01:16 -0600

    finished

--- script.js
+++ script.js
@@ -9,15 +9,29 @@
 
  */
 
+// Function to get values from text input
+getValues = function (){
 
-//Plus operation
+    number1 = document.getElementById("number1").value;
+    number2 = document.getElementById("number2").value;
+
+    resultObject = {
+        "number1": number1,
+        "number2": number2
+    }
+    return resultObject;
+
+}
+
+
+
+//Plus operation - Used with Shortend code of getValues function
 plusButton = document.getElementById("plusButton");
 plusOperation = function(){
 
-    number1 = document.getElementById("number1");
-    number2 = document.getElementById("number2");
+    numberValues = getValues();
 
-    sum = parseInt(number1.value) + parseInt(number2.value);
+    sum = parseInt(numberValues.number1) + parseInt(numberValues.number2);
 
     result = document.getElementById("result");
     result.innerText = sum; 
@@ -25,3 +39,58 @@ plusOperation = function(){
     
 }
 plusButton.addEventListener("click", plusOperation );
+
+// Minus operations
+
+minusButton = document.getElementById("minusButton");
+
+minusOperation = function (){
+
+    number1 = document.getElementById("number1");
+    number2 = document.getElementById("number2");
+
+    difference = parseInt(number1.value) - parseInt(number2.value);
+    result.innerText = difference;
+
+
+
+}
+
+minusButton.addEventListener("click", minusOperation );
+
+
+// Multiplication Operations
+
+mulButton = document.getElementById("mulButton");
+
+mulOperation = function (){
+
+    number1 = document.getElementById("number1");
+    number2 = document.getElementById("number2");
+
+    multiple = parseInt(number1.value) * parseInt(number2.value);
+    result.innerText = multiple;
+
+}
+
+
+mulButton.addEventListener("click", mulOperation)
+
+
+// Division Operation
+
+divButton = document.getElementById("divButton");
+
+divOperation = function (){
+
+    number1 = document.getElementById("number1");
+    number2 = document.getElementById("number2");
+
+    division = parseInt(number1.value) / parseInt(number2.value);
+    result.innerText = division;
+
+}
+
+
+divButton.addEventListener("click", divOperation);
+
